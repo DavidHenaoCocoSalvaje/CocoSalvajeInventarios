8722ed45d26a15a065eebf8723b17d349f5cd350
DavidHHDS <david.henao.ds@outlook.com>
2025-07-18 09:06:38 -0500

    feat: Iniciar querys de inventarios

--- app/internal/query/inventario.py
+++ app/internal/query/inventario.py
@@ -1,24 +1,13 @@
 # app/internal/query/inventario.py
-from sqlalchemy.ext.asyncio import AsyncSession
-
-from sqlmodel import select
-
-from app.models.usuario import UsuarioDB
+import app.models.inventario as inv
 from app.internal.query.base import BaseQuery
 
 
-class UsuarioQuery(BaseQuery[UsuarioDB]):
+class ElementoInventarioQuery(BaseQuery[inv.ElementoInventario]):
     """Repositorio para la entidad Usuario"""
 
     def __init__(self):
-        super().__init__(UsuarioDB)
-
-    async def get_by_username(self, session: AsyncSession, username: str):
-        """Obtiene un usuario por su documento de identidad"""
-        statement = select(self.model).where(self.model.username == username)
-        results = await session.execute(statement)
-        scalar_results = results.scalars()
-        return scalar_results.one_or_none()
+        super().__init__(inv.ElementoInventario)
 
 
-usuario_query = UsuarioQuery()
+elemento_inventario_query = ElementoInventarioQuery()
